382a9423551115f4724ff6d530173bffc2373df5
Fagun Bhavsar <github.fagun@gmail.com>
2020-07-02 14:43:02 +0530

    Problem - yourschedule

---
+++ YourSchedule.java
@@ -0,0 +1,20 @@
+public class YourSchedule
+{
+	public static void main(String[] args){
+		String course1 = "English";
+		String teacher1 = "Mrs Floyd";
+		String course2 = "Biology";
+		String teacher2 = "Mrs Rachana";
+		String course3 = "Maths";
+		String teacher3 = "Mrs Morrison";
+		String course4 = "Physics";
+		String teacher4 = "Mrs Robinson";
+
+		System.out.println("------------------------");
+		System.out.println(course1+ "     " +teacher1);
+		System.out.println(course2+ "     " +teacher2);
+		System.out.println(course3+ "       " +teacher3);
+		System.out.println(course4+ "     " +teacher4);
+		System.out.println("------------------------");
+	}
+}
